d076672078f43560ff696f94e9adc2a2309d7e56
Zozimo Fernandez <zozi.fer96@gmail.com>
2021-03-20 00:34:49 -0600

    Modal funcional
    Nos falta mostrar los datos instanciados

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import BookListView, BookCreateView
+from .views import BookListView, BookCreateView, BookUpdateView
 
 app_name = 'books'
 
 urlpatterns = [
     path('', BookListView.as_view(), name='list'),
     path('create/', BookCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', BookUpdateView.as_view(), name='update'),
 ]
--- apps/books/views.py
+++ apps/books/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView
 
 from .models import Book
 from .forms import BookForm
@@ -24,9 +24,23 @@ class BookCreateView(CreateView):
     model = Book
     form_class = BookForm
     success_url = reverse_lazy('books:list')
+    template_name = 'books/create.html'
 
     def get_context_data(self, **kwargs):
         context = super(BookCreateView, self).get_context_data(**kwargs)
+        context['form'] = self.form_class
+        context['title'] = 'Registrar libro'
+        return context
+
+
+class BookUpdateView(UpdateView):
+    model = Book
+    form_class = BookForm
+    success_url = reverse_lazy('books:list')
+    template_name = 'books/includes/modal_edit.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(BookUpdateView, self).get_context_data(**kwargs)
         context['form'] = self.form_class
         context['title'] = 'Registrar libro'
         return context
